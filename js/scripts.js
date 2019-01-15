ecc57567854c828e55f6c1cc1676e37f63002750
Pavel Zanchuk and Tristan Setha <tristan@>
2019-01-15 14:27:49 -0800

    added 3 interactive flashcards

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("#JS").click(function() {
+    $("#p-hidden").fadeToggle();
+    $("#p-showing").fadeToggle();
+
+  });
+
+  $("#VR").click(function() {
+    $("#p2-hidden").fadeToggle();
+    $("#p2-showing").fadeToggle();
+
+  });
+
+  $("#Var").click(function() {
+    $("#p3-hidden").fadeToggle();
+    $("#p3-showing").fadeToggle();
+
+  });
+});
